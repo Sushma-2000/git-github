709e976ddd2e211a4ff34291643238276415abc0
su101 <janamanchisushma.101@gmail.com>
2020-09-20 15:05:22 +0530

    I am adding bst program

---
+++ ISBST.java
@@ -0,0 +1,108 @@
+package p;
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+
+class Nodes{
+    int data;
+    Nodes left;
+    Nodes right;
+    Nodes(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class ISBST{
+    
+    static Nodes buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Nodes root = new Nodes(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Nodes> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Nodes currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Nodes(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Nodes(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Nodes root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Nodes root = buildTree(s);
+        	    Trees g = new Trees();
+        	    if(g.isBST(root))
+			        System.out.println(1);
+			    else
+			        System.out.println(0);
+                t--;
+            
+        }
+    }
+  
+}
+
